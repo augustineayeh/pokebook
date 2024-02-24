474925004ebcddeedb7078d3b89ce223e8654e4e
Augustine <augyog07@gmail.com>
2024-02-24 12:55:48 +0000

    add constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color pink = Color(0xffe55a85);
+  static const Color amber = Color(0xffdaa429);
+  static const Color lightBlue = Color(0xff3fb8db);
+  static const Color lightGrey = Color(0xfff1f1f1);
+  static const Color lightGreen = Color(0xff4ba8b1);
+}
---
+++ lib/core/constants/text_constants.dart
@@ -0,0 +1,6 @@
+class TextConstants {
+  TextConstants._();
+
+  static const String homePageText =
+      'Largest Pokémon index with information about every Pokemon you can think of.';
+}
